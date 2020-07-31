1ff66af2b3886b45473d192c3297f18ef51a12b5
Angelo Stavrow <contact@angelostavrow.com>
2020-07-31 15:14:53 -0400

    Add basic PostCollection type

---
+++ Shared/PostCollection/PostCollection.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct PostCollection: Identifiable {
+    let id = UUID()
+    let title: String
+}
+
+let defaultDraftCollection = PostCollection(title: "Drafts")
+let testPostCollection1 = PostCollection(title: "Collection 1")
+let testPostCollection2 = PostCollection(title: "Collection 2")
+let testPostCollection3 = PostCollection(title: "Collection 3")
