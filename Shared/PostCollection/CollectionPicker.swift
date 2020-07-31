97a02b8530489091628fedcee3363b7f02180c84
Angelo Stavrow <contact@angelostavrow.com>
2020-07-31 16:26:37 -0400

    Add CollectionPicker view for testing collection-selection functionality

---
+++ Shared/PostCollection/CollectionPicker.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct CollectionPicker: View {
+    @Binding var selectedCollection: PostCollection
+
+    private let collections = [
+        defaultDraftCollection,
+        testPostCollection1,
+        testPostCollection2,
+        testPostCollection3
+    ]
+
+    var body: some View {
+        Picker("Collection", selection: $selectedCollection) {
+            ForEach(collections) { collection in
+                Text(collection.title).tag(collection)
+            }
+        }
+    }
+}
