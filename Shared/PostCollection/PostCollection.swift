ab0a89ba079d29b14f660e9c01f6603b61dc54a6
Angelo Stavrow <contact@angelostavrow.com>
2020-07-31 16:53:48 -0400

    Add an "all posts" collection

--- Shared/PostCollection/PostCollection.swift
+++ Shared/PostCollection/PostCollection.swift
@@ -5,6 +5,7 @@ struct PostCollection: Identifiable, Hashable {
     let title: String
 }
 
+let allPostsCollection = PostCollection(title: "All Posts")
 let defaultDraftCollection = PostCollection(title: "Drafts")
 let testPostCollection1 = PostCollection(title: "Collection 1")
 let testPostCollection2 = PostCollection(title: "Collection 2")
