4d4e4be1628d3e2e9b604e0baca29d1224db7540
Angelo Stavrow <contact@angelostavrow.com>
2020-08-10 12:38:30 -0400

    Adapt PreferencesView to embed in iOS SettingsView form

--- Shared/Preferences/PreferencesView.swift
+++ Shared/Preferences/PreferencesView.swift
@@ -4,6 +4,14 @@ struct PreferencesView: View {
     @State private var appearance: Int = 0
 
     var body: some View {
+        #if os(iOS)
+        Picker(selection: $appearance, label: Text("Appearance")) {
+            Text("System").tag(0)
+            Text("Light Mode").tag(1)
+            Text("Dark Mode").tag(2)
+        }
+        .pickerStyle(SegmentedPickerStyle())
+        #elseif os(macOS)
         Form {
             Picker(selection: $appearance, label: Text("Appearance")) {
                 Text("System").tag(0)
@@ -13,6 +21,7 @@ struct PreferencesView: View {
             .frame(width: 200, height: 100, alignment: .topLeading)
             .pickerStyle(RadioGroupPickerStyle())
         }
+        #endif
     }
 }
 
