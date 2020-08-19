1bdd9189317c2f4c0be1fc3aa3025a75232a8088
Angelo Stavrow <contact@angelostavrow.com>
2020-08-19 16:33:16 -0400

    Add localizedError description to AccountError cases

--- Shared/Account/AccountModel.swift
+++ Shared/Account/AccountModel.swift
@@ -7,6 +7,28 @@ enum AccountError: Error {
     case serverNotFound
 }
 
+extension AccountError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .serverNotFound:
+            return NSLocalizedString(
+                "The server could not be found. Please check the information you've entered and try again.",
+                comment: ""
+            )
+        case .invalidPassword:
+            return NSLocalizedString(
+                "Invalid password. Please check that you've entered your password correctly and try logging in again.",
+                comment: ""
+            )
+        case .usernameNotFound:
+            return NSLocalizedString(
+                "Username not found. Did you use your email address by mistake?",
+                comment: ""
+            )
+        }
+    }
+}
+
 struct AccountModel {
     var server: String = ""
     private(set) var user: WFUser?
