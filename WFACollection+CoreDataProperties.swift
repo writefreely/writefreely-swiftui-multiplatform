f235a0c2d054d7da8c6c8562caeb53745d48328b
Angelo Stavrow <contact@angelostavrow.com>
2020-09-02 11:14:19 -0400

    Add uniqueness constraint on alias, remove views attribute

--- WFACollection+CoreDataProperties.swift
+++ WFACollection+CoreDataProperties.swift
@@ -8,16 +8,15 @@ extension WFACollection {
     }
 
     @NSManaged public var alias: String?
-    @NSManaged public var title: String?
     @NSManaged public var blogDescription: String?
-    @NSManaged public var styleSheet: String?
-    @NSManaged public var isPublic: Bool
-    @NSManaged public var views: Int16
     @NSManaged public var email: String?
+    @NSManaged public var isPublic: Bool
+    @NSManaged public var styleSheet: String?
+    @NSManaged public var title: String?
     @NSManaged public var url: String?
 
 }
 
-extension WFACollection: Identifiable {
+extension WFACollection : Identifiable {
 
 }
