3bdc08f06793097b16db56277debc52b1163e8a4
Angelo Stavrow <contact@angelostavrow.com>
2020-09-04 16:55:13 -0400

    Add WFAPost NSManagedObject class for local storage

---
+++ WFAPost+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+import Foundation
+import CoreData
+
+extension WFAPost {
+
+    @nonobjc public class func createFetchRequest() -> NSFetchRequest<WFAPost> {
+        return NSFetchRequest<WFAPost>(entityName: "WFAPost")
+    }
+
+    @NSManaged public var appearance: String?
+    @NSManaged public var body: String?
+    @NSManaged public var collectionAlias: String?
+    @NSManaged public var createdDate: Date?
+    @NSManaged public var language: String?
+    @NSManaged public var postId: String?
+    @NSManaged public var rtl: Bool
+    @NSManaged public var slug: String?
+    @NSManaged public var status: Int32
+    @NSManaged public var title: String?
+    @NSManaged public var updatedDate: Date?
+
+}
+
+extension WFAPost: Identifiable {
+
+}
