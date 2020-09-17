edec5e908702dd5530eae755748a92dc0aba6a5d
Angelo Stavrow <contact@angelostavrow.com>
2020-09-17 10:34:48 -0400

    Delete local post via swipe

--- Shared/PostList/PostListFilteredView.swift
+++ Shared/PostList/PostListFilteredView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct PostListFilteredView: View {
     @EnvironmentObject var model: WriteFreelyModel
+
     var fetchRequest: FetchRequest<WFAPost>
 
     init(filter: String?, showAllPosts: Bool) {
@@ -28,23 +29,33 @@ struct PostListFilteredView: View {
     }
 
     var body: some View {
-        List(fetchRequest.wrappedValue, id: \.self) { post in
-            NavigationLink(
-                destination: PostEditorView(post: post),
-                tag: post,
-                selection: $model.selectedPost
-            ) {
-                PostCellView(post: post)
-            }
+        List {
+            ForEach(fetchRequest.wrappedValue, id: \.self) { post in
+                NavigationLink(
+                    destination: PostEditorView(post: post),
+                    tag: post,
+                    selection: $model.selectedPost
+                ) {
+                    PostCellView(post: post)
+                }
+            }.onDelete(perform: { indexSet in
+                for index in indexSet {
+                    let post = fetchRequest.wrappedValue[index]
+                    if post.status == PostStatus.local.rawValue {
+                        delete(post)
+                    }
+                }
+            })
         }
+    }
+
+    func delete(_ post: WFAPost) {
+        model.posts.remove(post)
     }
 }
 
 struct PostListFilteredView_Previews: PreviewProvider {
     static var previews: some View {
-        let context = LocalStorageManager.persistentContainer.viewContext
-
         return PostListFilteredView(filter: nil, showAllPosts: false)
-            .environment(\.managedObjectContext, context)
     }
 }
