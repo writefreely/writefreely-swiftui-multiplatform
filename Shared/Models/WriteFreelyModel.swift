bc77fa14af6dfcc09a24e02e35651b60630dccdb
Angelo Stavrow <contact@angelostavrow.com>
2020-09-23 16:15:08 -0400

    On launch, either restore lastDraft or go to new blank post

--- Shared/Models/WriteFreelyModel.swift
+++ Shared/Models/WriteFreelyModel.swift
@@ -57,6 +57,32 @@ class WriteFreelyModel: ObservableObject {
                 self.fetchUserCollections()
                 self.fetchUserPosts()
             }
+
+            if let lastDraft = self.editor.fetchLastDraft() {
+                self.selectedPost = lastDraft
+            } else {
+                let managedPost = WFAPost(context: LocalStorageManager.persistentContainer.viewContext)
+                managedPost.createdDate = Date()
+                managedPost.title = ""
+                managedPost.body = ""
+                managedPost.status = PostStatus.local.rawValue
+                switch self.preferences.font {
+                case 1:
+                    managedPost.appearance = "sans"
+                case 2:
+                    managedPost.appearance = "wrap"
+                default:
+                    managedPost.appearance = "serif"
+                }
+                if let languageCode = Locale.current.languageCode {
+                    managedPost.language = languageCode
+                    managedPost.rtl = Locale.characterDirection(forLanguage: languageCode) == .rightToLeft
+                }
+                DispatchQueue.main.async {
+                    LocalStorageManager().saveContext()
+                }
+                self.selectedPost = managedPost
+            }
         }
 
         monitor.pathUpdateHandler = { path in
