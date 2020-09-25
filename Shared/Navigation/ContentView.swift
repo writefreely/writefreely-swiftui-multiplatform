a1cc1a5fc36e50c52f73c707206d132fc1a49af8
Angelo Stavrow <contact@angelostavrow.com>
2020-09-25 16:29:32 -0400

    Make the post editor loads in all app-launch/make-active situations

--- Shared/Navigation/ContentView.swift
+++ Shared/Navigation/ContentView.swift
@@ -3,6 +3,14 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var model: WriteFreelyModel
 
+    #if os(iOS)
+    let didFinishLaunchingNotification = UIApplication.didFinishLaunchingNotification
+    let didBecomeActiveNotification = UIApplication.didBecomeActiveNotification
+    #elseif os(macOS)
+    let didFinishLaunchingNotification = NSApplication.didFinishLaunchingNotification
+    let didBecomeActiveNotification = NSApplication.didBecomeActiveNotification
+    #endif
+
     var body: some View {
         NavigationView {
             SidebarView()
@@ -12,30 +20,19 @@ struct ContentView: View {
             Text("Select a post, or create a new local draft.")
                 .foregroundColor(.secondary)
         }
-        .onAppear(perform: {
-            if let lastDraft = self.model.editor.fetchLastDraft() {
-                model.selectedPost = lastDraft
-            } else {
-                let managedPost = WFAPost(context: LocalStorageManager.persistentContainer.viewContext)
-                managedPost.createdDate = Date()
-                managedPost.title = ""
-                managedPost.body = ""
-                managedPost.status = PostStatus.local.rawValue
-                switch self.model.preferences.font {
-                case 1:
-                    managedPost.appearance = "sans"
-                case 2:
-                    managedPost.appearance = "wrap"
-                default:
-                    managedPost.appearance = "serif"
-                }
-                if let languageCode = Locale.current.languageCode {
-                    managedPost.language = languageCode
-                    managedPost.rtl = Locale.characterDirection(forLanguage: languageCode) == .rightToLeft
-                }
-                model.selectedPost = managedPost
-            }
-        })
+        .onReceive(NotificationCenter.default.publisher(for: didFinishLaunchingNotification)) { _ in
+            #if os(macOS)
+            launchToEditor()
+            #endif
+        }
+        .onReceive(NotificationCenter.default.publisher(for: didBecomeActiveNotification)) { _ in
+            launchToEditor()
+        }
+        .onAppear {
+            #if os(iOS)
+            launchToEditor()
+            #endif
+        }
         .environmentObject(model)
         .alert(isPresented: $model.isPresentingDeleteAlert) {
             Alert(
@@ -67,6 +64,35 @@ struct ContentView: View {
                 }
             )
         #endif
+    }
+
+    private func launchToEditor() {
+        if let lastDraft = self.model.editor.fetchLastDraft() {
+            DispatchQueue.main.async {
+                model.selectedPost = lastDraft
+            }
+        } else {
+            let managedPost = WFAPost(context: LocalStorageManager.persistentContainer.viewContext)
+            managedPost.createdDate = Date()
+            managedPost.title = ""
+            managedPost.body = ""
+            managedPost.status = PostStatus.local.rawValue
+            switch self.model.preferences.font {
+            case 1:
+                managedPost.appearance = "sans"
+            case 2:
+                managedPost.appearance = "wrap"
+            default:
+                managedPost.appearance = "serif"
+            }
+            if let languageCode = Locale.current.languageCode {
+                managedPost.language = languageCode
+                managedPost.rtl = Locale.characterDirection(forLanguage: languageCode) == .rightToLeft
+            }
+            DispatchQueue.main.async {
+                model.selectedPost = managedPost
+            }
+        }
     }
 }
 
