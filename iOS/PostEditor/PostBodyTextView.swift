3b88d45841a26cd584a44cee8ed54ecb6a4687c6
Angelo Stavrow <contact@angelostavrow.com>
2020-10-27 14:30:21 -0400

    Add placeholder files for UIViewRepresentable replacement of TextEditor

---
+++ iOS/PostEditor/PostBodyTextView.swift
@@ -0,0 +1,20 @@
+//
+//  PostBodyTextView.swift
+//  WriteFreely-MultiPlatform (iOS)
+//
+//  Created by Angelo Stavrow on 2020-10-27.
+//
+
+import SwiftUI
+
+struct PostBodyTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostBodyTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostBodyTextView()
+    }
+}
---
+++ iOS/PostEditor/PostTitleTextView.swift
@@ -0,0 +1,20 @@
+//
+//  PostTitleTextView.swift
+//  WriteFreely-MultiPlatform (iOS)
+//
+//  Created by Angelo Stavrow on 2020-10-27.
+//
+
+import SwiftUI
+
+struct PostTitleTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostTitleTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostTitleTextView()
+    }
+}
