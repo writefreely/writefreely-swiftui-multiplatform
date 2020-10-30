4ef40453d5483d09f32bae6b2ebd9bd148ab8ea2
Angelo Stavrow <contact@angelostavrow.com>
2020-10-30 15:26:32 -0400

    Add methods to save, clear, and retrieve last draft from UserDefaults

--- Shared/PostEditor/PostEditorModel.swift
+++ Shared/PostEditor/PostEditorModel.swift
@@ -1,4 +1,4 @@
-import Foundation
+import SwiftUI
 import CoreData
 
 enum PostAppearance: String {
@@ -8,5 +8,25 @@ enum PostAppearance: String {
 }
 
 struct PostEditorModel {
+    @AppStorage("lastDraftURL") private var lastDraftURL: URL?
 
+    func saveLastDraft(_ post: WFAPost) {
+        self.lastDraftURL = post.status != PostStatus.published.rawValue ? post.objectID.uriRepresentation() : nil
+    }
+
+    func clearLastDraft() {
+        self.lastDraftURL = nil
+    }
+
+    func fetchLastDraftFromUserDefaults() -> WFAPost? {
+        guard let postURL = lastDraftURL else { return nil }
+
+        let coordinator = LocalStorageManager.persistentContainer.persistentStoreCoordinator
+        guard let postManagedObjectID = coordinator.managedObjectID(forURIRepresentation: postURL) else { return nil }
+        guard let post = LocalStorageManager.persistentContainer.viewContext.object(
+                with: postManagedObjectID
+        ) as? WFAPost else { return nil }
+
+        return post
+    }
 }
