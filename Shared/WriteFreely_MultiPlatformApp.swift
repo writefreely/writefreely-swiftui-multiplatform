444859b2fba73457be6c912655291cbd7d0e095b
Angelo Stavrow <contact@angelostavrow.com>
2020-11-04 15:23:03 -0500

    Load last draft or new post on launch

--- Shared/WriteFreely_MultiPlatformApp.swift
+++ Shared/WriteFreely_MultiPlatformApp.swift
@@ -11,6 +11,13 @@ struct WriteFreely_MultiPlatformApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear(perform: {
+                    if let lastDraft = model.editor.fetchLastDraftFromUserDefaults() {
+                        self.model.selectedPost = lastDraft
+                    } else {
+                        createNewLocalPost()
+                    }
+                })
                 .environmentObject(model)
                 .environment(\.managedObjectContext, LocalStorageManager.persistentContainer.viewContext)
 //                .preferredColorScheme(preferences.selectedColorScheme)    // See PreferencesModel for info.
@@ -38,5 +45,27 @@ struct WriteFreely_MultiPlatformApp: App {
 //            .preferredColorScheme(preferences.selectedColorScheme)    // See PreferencesModel for info.
         }
         #endif
+    }
+
+    private func createNewLocalPost() {
+        let managedPost = WFAPost(context: LocalStorageManager.persistentContainer.viewContext)
+        managedPost.createdDate = Date()
+        managedPost.title = ""
+        managedPost.body = ""
+        managedPost.status = PostStatus.local.rawValue
+        managedPost.collectionAlias = nil
+        switch model.preferences.font {
+        case 1:
+            managedPost.appearance = "sans"
+        case 2:
+            managedPost.appearance = "wrap"
+        default:
+            managedPost.appearance = "serif"
+        }
+        if let languageCode = Locale.current.languageCode {
+            managedPost.language = languageCode
+            managedPost.rtl = Locale.characterDirection(forLanguage: languageCode) == .rightToLeft
+        }
+        self.model.selectedPost = managedPost
     }
 }
