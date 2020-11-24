1215a1727a826efd9b4541df5053b7b9792ea548
Angelo Stavrow <contact@angelostavrow.com>
2020-11-24 15:24:08 -0500

    Remove PostListView toolbar (new post and reload from server)

--- Shared/PostList/PostListView.swift
+++ Shared/PostList/PostListView.swift
@@ -61,55 +61,7 @@ struct PostListView: View {
                 )
             )
             .navigationSubtitle(postCount == 1 ? "\(postCount) post" : "\(postCount) posts")
-            .toolbar {
-                Button(action: {
-                    createNewLocalDraft()
-                }, label: {
-                    Image(systemName: "square.and.pencil")
-                })
-                Button(action: {
-                    reloadFromServer()
-                }, label: {
-                    Image(systemName: "arrow.clockwise")
-                })
-                .disabled(!model.account.isLoggedIn)
-            }
         #endif
-    }
-
-    private func reloadFromServer() {
-        DispatchQueue.main.async {
-            model.fetchUserCollections()
-            model.fetchUserPosts()
-        }
-    }
-
-    private func createNewLocalDraft() {
-        let managedPost = WFAPost(context: self.managedObjectContext)
-        managedPost.createdDate = Date()
-        managedPost.title = ""
-        managedPost.body = ""
-        managedPost.status = PostStatus.local.rawValue
-        managedPost.collectionAlias = nil
-        switch model.preferences.font {
-        case 1:
-            managedPost.appearance = "sans"
-        case 2:
-            managedPost.appearance = "wrap"
-        default:
-            managedPost.appearance = "serif"
-        }
-        if let languageCode = Locale.current.languageCode {
-            managedPost.language = languageCode
-            managedPost.rtl = Locale.characterDirection(forLanguage: languageCode) == .rightToLeft
-        }
-        DispatchQueue.main.async {
-            self.selectedCollection = nil
-            self.showAllPosts = false
-            withAnimation {
-                self.model.selectedPost = managedPost
-            }
-        }
     }
 }
 
