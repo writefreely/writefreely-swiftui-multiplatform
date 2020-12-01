43088fdc44582a5a324aaf1206ea403c46d431a4
Angelo Stavrow <contact@angelostavrow.com>
2020-12-01 10:01:56 -0500

    Swap order of share and publish buttons in toolbar

--- macOS/Navigation/ActivePostToolbarView.swift
+++ macOS/Navigation/ActivePostToolbarView.swift
@@ -8,10 +8,10 @@ struct ActivePostToolbarView: View {
         HStack(spacing: 16) {
             PostEditorStatusToolbarView(post: activePost)
             HStack(spacing: 4) {
-                Button(action: { publishPost(activePost) }, label: { Image(systemName: "paperplane") })
-                    .disabled(activePost.body.isEmpty || activePost.status == PostStatus.published.rawValue)
                 Button(action: {}, label: { Image(systemName: "square.and.arrow.up") })
                     .disabled(activePost.status == PostStatus.local.rawValue)
+                Button(action: { publishPost(activePost) }, label: { Image(systemName: "paperplane") })
+                    .disabled(activePost.body.isEmpty || activePost.status == PostStatus.published.rawValue)
             }
         }
     }
