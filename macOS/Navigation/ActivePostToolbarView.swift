f52fae335b648234fd7b7298d9763be625638fa8
Angelo Stavrow <contact@angelostavrow.com>
2020-12-15 14:53:18 -0500

    Present publish-to menu for new local posts

--- macOS/Navigation/ActivePostToolbarView.swift
+++ macOS/Navigation/ActivePostToolbarView.swift
@@ -12,8 +12,10 @@ struct ActivePostToolbarView: View {
     ) var collections: FetchedResults<WFACollection>
 
     var body: some View {
-        HStack(spacing: 4) {
-            if model.account.isLoggedIn && activePost.status != PostStatus.local.rawValue {
+        HStack {
+            if model.account.isLoggedIn &&
+                activePost.status != PostStatus.local.rawValue &&
+                !(activePost.wasDeletedFromServer || activePost.hasNewerRemoteCopy) {
                 Section(header: Text("Move To:")) {
                     Picker(selection: $selectedCollection, label: Text("Move To…"), content: {
                         Text("\(model.account.server == "https://write.as" ? "Anonymous" : "Drafts")")
@@ -26,12 +28,51 @@ struct ActivePostToolbarView: View {
                 }
             }
             PostEditorStatusToolbarView(post: activePost)
+                .frame(minWidth: 50, alignment: .center)
                 .layoutPriority(1)
                 .padding(.horizontal)
-            Button(action: {}, label: { Image(systemName: "square.and.arrow.up") })
-                .disabled(activePost.status == PostStatus.local.rawValue)
-            Button(action: { publishPost(activePost) }, label: { Image(systemName: "paperplane") })
-                .disabled(activePost.body.isEmpty || activePost.status == PostStatus.published.rawValue)
+            if activePost.status == PostStatus.local.rawValue {
+                Menu(content: {
+                    Label("Publish To:", systemImage: "paperplane")
+                    Divider()
+                    Button(action: {
+                        if model.account.isLoggedIn {
+                            withAnimation {
+                                activePost.collectionAlias = nil
+                                publishPost(activePost)
+                            }
+                        } else {
+                            // present login screen
+                        }
+                    }, label: {
+                        Text("\(model.account.server == "https://write.as" ? "Anonymous" : "Drafts")")
+                    })
+                    ForEach(collections) { collection in
+                        Button(action: {
+                            if model.account.isLoggedIn {
+                                withAnimation {
+                                    activePost.collectionAlias = collection.alias
+                                    publishPost(activePost)
+                                }
+                            } else {
+                                // present login screen
+                            }
+                        }, label: {
+                            Text("\(collection.title)")
+                        })
+                    }
+                }, label: {
+                    Label("Publish…", systemImage: "paperplane")
+                })
+                .disabled(activePost.body.isEmpty)
+            } else {
+                HStack(spacing: 4) {
+                    Button(action: {}, label: { Image(systemName: "square.and.arrow.up") })
+                        .disabled(activePost.status == PostStatus.local.rawValue)
+                    Button(action: { publishPost(activePost) }, label: { Image(systemName: "paperplane") })
+                        .disabled(activePost.body.isEmpty || activePost.status == PostStatus.published.rawValue)
+                }
+            }
         }
         .onAppear(perform: {
             self.selectedCollection = collections.first { $0.alias == activePost.collectionAlias }
