a9eea94d4eece284ffb001763e8509053b96318c
Angelo Stavrow <contact@angelostavrow.com>
2020-12-22 17:02:56 -0500

    Stub out basic checking for updates in AppDelegate

--- macOS/AppDelegate.swift
+++ macOS/AppDelegate.swift
@@ -1,7 +1,13 @@
 import Cocoa
+import Sparkle
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
-        
+        SUUpdater.shared()?.automaticallyChecksForUpdates = true
+        /*
+         Next line prints:
+         ⚠️ You must specify the URL of the appcast as the SUFeedURL key in either the Info.plist or the user defaults!
+         */
+        SUUpdater.shared()?.checkForUpdates(self)
     }
 }
