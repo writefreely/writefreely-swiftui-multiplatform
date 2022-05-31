b1a8b8b29c7a745ea9733ed0d7266c775b534b47
Angelo Stavrow <contact@angelostavrow.com>
2022-05-31 07:23:41 -0400

    Add error handling to Mac app

--- macOS/Settings/MacAccountView.swift
+++ macOS/Settings/MacAccountView.swift
@@ -2,11 +2,22 @@ import SwiftUI
 
 struct MacAccountView: View {
     @EnvironmentObject var model: WriteFreelyModel
+    @EnvironmentObject var errorHandling: ErrorHandling
 
     var body: some View {
-            Form {
-                    AccountView()
+        Form {
+            AccountView()
+        }
+        .onChange(of: model.hasError) { value in
+            if value {
+                if let error = model.currentError {
+                    self.errorHandling.handle(error: error)
+                } else {
+                    self.errorHandling.handle(error: AppError.genericError())
+                }
+                model.hasError = false
             }
+        }
     }
 }
 
