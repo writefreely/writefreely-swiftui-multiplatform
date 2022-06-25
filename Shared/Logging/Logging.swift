b93e0c3547e23db53e464a57e2d8df469ae319b2
Angelo Stavrow <contact@angelostavrow.com>
2022-06-25 12:07:38 -0400

    Refactor class to use protocol

--- Shared/Logging/Logging.swift
+++ Shared/Logging/Logging.swift
@@ -7,15 +7,25 @@
 
 import Foundation
 import os
+import OSLog
+
+protocol LogWriter {
+    func log(_ message: String, withSensitiveInfo privateInfo: String?, level: OSLogType)
+    func logCrashAndSetFlag(error: Error)
+}
 
 final class Logging {
 
     private let logger: Logger
     private let subsystem = Bundle.main.bundleIdentifier!
 
-    init(for category: String) {
+    init(for category: String = "") {
         self.logger = Logger(subsystem: subsystem, category: category)
     }
+
+}
+
+extension Logging: LogWriter {
 
     func log(
         _ message: String,
@@ -33,7 +43,7 @@ final class Logging {
         let errorDescription = error.localizedDescription
         UserDefaults.shared.set(true, forKey: WFDefaults.didHaveFatalError)
         UserDefaults.shared.set(errorDescription, forKey: WFDefaults.fatalErrorDescription)
-        logger.critical("\(errorDescription)")
+        logger.log(level: .error, "\(errorDescription)")
         fatalError(errorDescription)
     }
 
