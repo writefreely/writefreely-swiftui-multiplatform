0d43cb0bd8e90a00f09853bc919c97ed36cecff2
Angelo Stavrow <angelo@droppedbits.com>
2022-09-07 08:29:13 -0400

    Prep for iOS 16 (#223)
    * Don’t render spacer in iOS 16+
    
    * Bump version and build for TestFlight
    
    * Bump version and build of action extension
    
    * Run SwiftLint on all files for each build
    
    This prevents a build warning in Xcode 14 because SwiftLint doesn’t yet support input/output files for these script execution build phases. It’s fast enough, so running it on all files isn’t an issue.
    
    * Add missing error handling on login view
    
    * Clean up line_length warning
    
    * Update change log

--- Shared/Account/AccountLoginView.swift
+++ Shared/Account/AccountLoginView.swift
@@ -88,6 +88,16 @@ struct AccountLoginView: View {
                 .padding()
             }
         }
+        .onChange(of: model.hasError) { value in
+            if value {
+                if let error = model.currentError {
+                    self.errorHandling.handle(error: error)
+                } else {
+                    self.errorHandling.handle(error: AppError.genericError())
+                }
+                model.hasError = false
+            }
+        }
     }
 }
 
