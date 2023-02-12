b4175168f0c96146526e4a7d8b0555d948318dcf
Angelo Stavrow <angelo@droppedbits.com>
2023-02-12 11:35:24 -0500

    Add “Create Log Post” command to Post menu

--- macOS/Navigation/PostCommands.swift
+++ macOS/Navigation/PostCommands.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct PostCommands: Commands {
     @ObservedObject var model: WriteFreelyModel
 
+    private let logger = Logging(for: String(describing: PostCommands.self))
+
     var body: some Commands {
         CommandMenu("Post") {
             Group {
@@ -10,6 +12,8 @@ struct PostCommands: Commands {
                     .disabled(model.selectedPost?.status == PostStatus.local.rawValue)
             }
             .disabled(model.selectedPost == nil || !model.account.isLoggedIn)
+
+            Button(action: createLogsPost, label: { Text("Create Log Post") })
         }
     }
 
@@ -21,5 +25,29 @@ struct PostCommands: Commands {
                 "\(model.account.server)/\((postId))" else { return }
         NSPasteboard.general.clearContents()
         NSPasteboard.general.setString(urlString, forType: .string)
+    }
+
+    private func createLogsPost() {
+       logger.log("Generating local log post...")
+
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+            // Unset selected post and collection and navigate to local drafts.
+            self.model.selectedPost = nil
+            self.model.selectedCollection = nil
+            self.model.showAllPosts = false
+
+            // Create the new log post.
+            let newLogPost = model.editor.generateNewLocalPost(withFont: 2)
+            newLogPost.title = "Logs For Support"
+            var postBody: [String] = [
+                "WriteFreely-Multiplatform v\(Bundle.main.appMarketingVersion) (\(Bundle.main.appBuildVersion))",
+                "Generated \(Date())",
+                ""
+            ]
+            postBody.append(contentsOf: logger.fetchLogs())
+            newLogPost.body = postBody.joined(separator: "\n")
+        }
+
+        logger.log("Generated local log post.")
     }
 }
