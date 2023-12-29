f65fea618ca3302117692624baa268e0b6d308df
Angelo Stavrow <angelo@droppedbits.com>
2023-12-29 09:56:33 -0500

    Testing some changes

--- Shared/Navigation/WFNavigation.swift
+++ Shared/Navigation/WFNavigation.swift
@@ -18,7 +18,8 @@ struct WFNavigation<CollectionList, PostList, PostDetail>: View
     }
 
     var body: some View {
-        if #available(iOS 16, macOS 13, *) {
+        #if os(macOS)
+        if #available(macOS 13, *) {
             NavigationSplitView {
                 collectionList
             } content: {
@@ -33,5 +34,24 @@ struct WFNavigation<CollectionList, PostList, PostDetail>: View
                 postDetail
             }
         }
+        #else
+        // If we uncomment this, when we back out of the postDetail to the postList, the app further backs us out
+        // to the collectionList for some reason.
+//        if #available(iOS 16, *) {
+//            NavigationSplitView {
+//                collectionList
+//            } content: {
+//                postList
+//            } detail: {
+//                postDetail
+//            }
+//        } else {
+            NavigationView {
+                collectionList
+                postList
+                postDetail
+            }
+//        }
+        #endif
     }
 }
--- Shared/PostList/SearchablePostListFilteredView.swift
+++ Shared/PostList/SearchablePostListFilteredView.swift
@@ -11,12 +11,12 @@ struct SearchablePostListFilteredView: View {
     var onDelete: (WFAPost) -> Void
 
     var body: some View {
-        List(selection: $model.selectedPost) {
-            ForEach(fetchRequest.wrappedValue, id: \.self) { post in
+        if #available(iOS 16, *) {
+            List(fetchRequest.wrappedValue, id: \.self, selection: $model.selectedPost) { post in
                 if !searchString.isEmpty &&
                     !post.title.localizedCaseInsensitiveContains(searchString) &&
                     !post.body.localizedCaseInsensitiveContains(searchString) {
-                        EmptyView()
+                    EmptyView()
                 } else {
                     NavigationLink(
                         destination: PostEditorView(post: post),
@@ -38,18 +38,48 @@ struct SearchablePostListFilteredView: View {
                     .deleteDisabled(post.status != PostStatus.local.rawValue)
                 }
             }
-            .onDelete(perform: { indexSet in
-                for index in indexSet {
-                    let post = fetchRequest.wrappedValue[index]
-                    delete(post)
+            .searchable(text: $searchString, prompt: "Search across posts")
+        } else {
+            List(selection: $model.selectedPost) {
+                ForEach(fetchRequest.wrappedValue, id: \.self) { post in
+                    if !searchString.isEmpty &&
+                        !post.title.localizedCaseInsensitiveContains(searchString) &&
+                        !post.body.localizedCaseInsensitiveContains(searchString) {
+                        EmptyView()
+                    } else {
+                        NavigationLink(
+                            destination: PostEditorView(post: post),
+                            tag: post,
+                            selection: $model.selectedPost,
+                            label: {
+                                if model.showAllPosts {
+                                    if let collection = collections.filter({ $0.alias == post.collectionAlias }).first {
+                                        PostCellView(post: post, collectionName: collection.title)
+                                    } else {
+                                        // swiftlint:disable:next line_length
+                                        let collectionName = model.account.server == "https://write.as" ? "Anonymous" : "Drafts"
+                                        PostCellView(post: post, collectionName: collectionName)
+                                    }
+                                } else {
+                                    PostCellView(post: post)
+                                }
+                            })
+                        .deleteDisabled(post.status != PostStatus.local.rawValue)
+                    }
                 }
-            })
+                .onDelete(perform: { indexSet in
+                    for index in indexSet {
+                        let post = fetchRequest.wrappedValue[index]
+                        delete(post)
+                    }
+                })
+            }
+            #if os(iOS)
+            .searchable(text: $searchString, prompt: "Search across posts")
+            #else
+            .searchable(text: $searchString, placement: .toolbar, prompt: "Search across posts")
+            #endif
         }
-        #if os(iOS)
-        .searchable(text: $searchString, prompt: "Search across posts")
-        #else
-        .searchable(text: $searchString, placement: .toolbar, prompt: "Search across posts")
-        #endif
     }
 
     func delete(_ post: WFAPost) {
