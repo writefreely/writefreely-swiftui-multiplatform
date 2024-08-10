5fbdc540e9a70fce3d85280aa020c748268e876a
Angelo Stavrow <angelo@droppedbits.com>
2024-08-10 10:40:52 -0400

    Cleanup and bump minimum iOS version to 15

--- Shared/Navigation/WFNavigation.swift
+++ Shared/Navigation/WFNavigation.swift
@@ -15,9 +15,14 @@ final class WFNavigationState: ObservableObject {
 struct WFNavigation<CollectionList, PostList, PostDetail>: View
     where CollectionList: View, PostList: View, PostDetail: View {
 
+    @EnvironmentObject var model: WriteFreelyModel
+    @FetchRequest(sortDescriptors: []) var collections: FetchedResults<WFACollection>
+
     private var collectionList: CollectionList
     private var postList: PostList
     private var postDetail: PostDetail
+
+    
 
     init(
         @ViewBuilder collectionList: () -> CollectionList,
@@ -43,10 +48,22 @@ struct WFNavigation<CollectionList, PostList, PostDetail>: View
             /// Consider converting this into a NavigationStack instead, and using `$model.selectedCollection` to set
             /// the detail view that should be shown. Try moving navigation state out of **WriteFreelyModel** and into
             /// **WFNavigation** instead, so that it eventually encapsulates _all_ things related to app navigation.
-            NavigationSplitView {
-                collectionList
-            } detail: {
-                postList
+//            NavigationSplitView {
+//                collectionList
+//            } detail: {
+//                postList
+//            }
+
+            NavigationStack {
+                List(collections, id: \.self, selection: $model.navState.selectedPost) { collection in
+                    NavigationLink("\(collection.title)", destination: PostListView(selectedCollection: model.navState.selectedCollection, showAllPosts: model.navState.showAllPosts))
+                }
+//                List(fetchRequest.wrappedValue, id: \.self, selection: $model.navState.selectedPost) { post in
+//                    NavigationLink(
+//                        "\(post.title.isEmpty ? "UNTITLED" : post.title)",
+//                        destination: PostEditorView(post: post)
+//                    )
+//                }
             }
         } else {
             NavigationView {
@@ -57,4 +74,5 @@ struct WFNavigation<CollectionList, PostList, PostDetail>: View
         }
         #endif
     }
+
 }
